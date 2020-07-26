4cd135708ded084d7e0b67ef446538f6c7d808dc
Mukulika <60316606+Mukulikaa@users.noreply.github.com>
2020-07-26 12:46:06 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -135,6 +135,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 # for production server
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
